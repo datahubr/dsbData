6c738382f3d2cf6bddf6085675a190b917842e01
datahubr <114759972+datahubr@users.noreply.github.com>
2022-09-30 16:42:20 +0100

    Add files via upload

---
+++ combi_cb.mli
@@ -0,0 +1,30 @@
+[This file contains specifications for the meta-analysis of SIENA projects.]
+[It serves as input for the SIENA08 program.]
+
+@1 [general information about SIENA projectlist <testCB>]
+19 [number of projects, names follow:]
+CB1
+CB3
+CB4
+CB6
+CB7
+CB8
+CB9
+CB10
+CB11
+CB13
+CB14
+CB15
+CB16
+CB18
+CB19
+CB20
+CB21
+CB22
+CB23
+
+@2 [options for estimation of projects]
+5 [upper bound for standard error in meta-analysis]
+1 [code 0=estimate, 1=aggregate from .out-files, 2=generate .dsc-file]
+1 [code 1=extra output] 
+0 [number of score tests]
